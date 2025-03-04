7d048be9fc593494a788594e8c20bea6542a9da5
Vinicio Sanchez-Mason <vinicio.sanchez@tempo.io>
2025-03-04 15:34:20 -0800

    one var and magic numbers

--- index.mjs
+++ index.mjs
@@ -39,6 +39,15 @@ export default tseslint.config(
             "react/jsx-no-bind": ["error", {
                 "allowArrowFunctions": true,
             }],
+            "no-magic-numbers": ["error", {
+                "enforceConst": true,
+                "detectObjects": true,
+            }],
+            "one-var": ["error", {
+                "var": "never",
+                "let": "never",
+                "const": "never",
+            }],
         },
     }
 );
